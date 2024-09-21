ab31b816c349b868e9ac75f5124d7f08a5fa4361
manojsvgit <manojsvgit@gmail.com>
2024-09-21 20:30:00 +0530

    added dark mode also

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,11 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/* TOGGLE BUTTON*/
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+  toggleButton.textContent = document.body.classList.contains('dark-theme') ? 'Light Theme' : 'Dark Theme';
+});
